df90f77f62b0c1c05efaa817fde94b06c3cbea81
aakash2410 <aakash.sangani2410@gmail.com>
2020-11-26 22:39:05 +0530

    Set up api using generic views

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -3,8 +3,8 @@ from rest_framework import generics
 from .models import Room
 from .serializers import RoomSerializer
 
-class RoomView(generics.CreateAPIView):
+class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
-    
+
 
