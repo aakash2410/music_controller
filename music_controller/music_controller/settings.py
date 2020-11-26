3bfc24fb12c9f6603b69be147ee0e6de9e6f4a1b
aakash2410 <aakash.sangani2410@gmail.com>
2020-11-26 23:52:29 +0530

    frontend setup properly and well tested.

--- music_controller/music_controller/settings.py
+++ music_controller/music_controller/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
-    'rest_framework'
+    'rest_framework',
+    'frontend.apps.FrontendConfig'
 
 ]
 
@@ -102,9 +103,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/3.1/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
