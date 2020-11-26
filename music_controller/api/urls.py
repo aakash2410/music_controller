886b9989fe9f4b2886e235823de43e52f34b2451
aakash2410 <aakash.sangani2410@gmail.com>
2020-11-27 01:06:03 +0530

    create-room api commit, session key determines host

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,7 +1,9 @@
-from .views import RoomView
+from .views import RoomView, CreateRoomView
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('home/', RoomView.as_view()),
+    path('create-room/', CreateRoomView.as_view()),
+    
 ]
