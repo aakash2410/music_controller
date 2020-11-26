b13ec18dcab00b18b1f9f12029c7a693d5e5a8bf
aakash2410 <aakash.sangani2410@gmail.com>
2020-11-27 00:38:29 +0530

    frontend React routing for various templates done

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
 from .models import Room
 from .serializers import RoomSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 
 class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
